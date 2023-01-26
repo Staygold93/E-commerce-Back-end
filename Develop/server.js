e064ac78310489caa9b6467199aea2f1365fd09a
Pharez <Staygold93@github.com>
2023-01-25 23:30:00 -0500

    added model data types

--- Develop/server.js
+++ Develop/server.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+

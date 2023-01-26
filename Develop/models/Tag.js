05cb513565381e7dd8148830a07786741bb0b196
Pharez <Staygold93@github.com>
2023-01-26 00:18:23 -0500

    added sequelize methods to models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
@@ -11,8 +11,12 @@ Tag.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      
-    }
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+
   },
   {
     sequelize,

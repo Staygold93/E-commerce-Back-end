028de459af4c638f4c203b3c81348b146464bca8
Pharez <Staygold93@github.com>
2023-01-28 20:30:14 -0500

    finished category routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection')
 // import sequelize connection
 
 const app = express();
@@ -7,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
